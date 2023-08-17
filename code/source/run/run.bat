59ae060fb1e57baf47fa388e35b473aa4345493f
Francesco Caporali <francescocaporali@outlook.com>
2023-08-17 12:11:28 +0200

    minor update
    portability file

--- code/source/run/run.bat
+++ code/source/run/run.bat
@@ -1,5 +1,4 @@
-@REM run on the venv khn_env_3.11 built on python 3.11.4
 @echo off
-powershell -ExecutionPolicy RemoteSigned -File .\run.ps1
+powershell -ExecutionPolicy Bypass -File .\run.ps1
 cmd /k
 exit /B 0
